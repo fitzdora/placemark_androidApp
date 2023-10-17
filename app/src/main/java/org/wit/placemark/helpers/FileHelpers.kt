eb6542372a1ac1b99df610dbac1211fc2be55a9a
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-10-17 17:32:42 +0100

    Adding Json and helper files

---
+++ app/src/main/java/org/wit/placemark/helpers/FileHelpers.kt
@@ -0,0 +1,53 @@
+package org.wit.placemark.helpers
+
+import android.content.Context
+import timber.log.Timber.Forest.e
+import java.io
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.lang.Exception
+
+fun write(context: Context, fileName: String, data: String){
+    try {
+        val outputStreamWriter = OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE))
+        outputStreamWriter.write(data)
+        outputStreamWriter.close()
+    } catch (e: Exception){
+        e("Cannot read file: %s", e.toString());
+    }
+}
+
+fun read(context: Context, fileName: String) : String {
+    var str = ""
+    try {
+        val inputStream = context.openFileInput(fileName)
+        if (inputStream != null){
+            val inputStreamReader = InputStreamReader(inputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            val partialStr = StringBuilder()
+            var done = false
+            while (!done){
+                val line = bufferedReader.readLine()
+                done = (line == null);
+                if (line != null) partialStr.append(line);
+            }
+            inputStream.close()
+            str = partialStr.toString()
+        }
+    } catch (e: FileNotFoundException) {
+        e("File not found: %s", e.toString());
+    } catch (e: IOException) {
+        e("Cannot read File: %s", e.toString());
+    }
+    return str
+
+}
+
+fun exist(context: Context, fileName: String) : Boolean {
+    val file = context.getFileStreamPath(fileName)
+    return  file.exists()
+
+}
--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -2,18 +2,19 @@ package org.wit.placemark.main
 
 import android.app.Application
 import org.wit.placemark.models.PlacemarkMemStore
-// import org.wit.placemark.models.PlacemarkModel
+import org.wit.placemark.models.PlacemarkModel
 import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class MainApp : Application() {
 
    // val placemarks = ArrayList<PlacemarkModel>()
-    val placemarks = PlacemarkMemStore()
+   lateinit var placemarks : PlacemarkMemStore
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        placemarks = PlacemarkMemStore()
         i("Placemark Started")
         /*placemarks.add(PlacemarkModel("One", "About one..."))
         placemarks.add(PlacemarkModel("Two", "About two..."))
